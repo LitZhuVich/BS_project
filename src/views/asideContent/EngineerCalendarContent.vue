87d4e8e515458cdc623c4b9a6273f89878c40deb
bilibili-CLY <lieyan123hcc@gmail.com>
2023-04-20 15:36:12 +0800

    工程师日历正在做

--- src/views/asideContent/EngineerCalendarContent.vue
+++ src/views/asideContent/EngineerCalendarContent.vue
@@ -15,55 +15,126 @@
 <script lang="ts" setup>
 import { cloneVNode } from 'vue'
 
-const generateColumns = (length = 10, prefix = 'column-', props?: any) =>
+const generateColumns = (length = 10, prefix = '号', props?: any) =>
   Array.from({ length }).map((_, columnIndex) => ({
     ...props,
-    key: `${prefix}${columnIndex}`,
-    dataKey: `${prefix}${columnIndex}`,
-    title: `Column ${columnIndex}`,
-    width: 150,
+    key: `${columnIndex}`,
+    dataKey: `${columnIndex}`,
+    title: `${columnIndex}${prefix}`,
+    width: 60,
   }))
 
 const generateData = (
   columns: ReturnType<typeof generateColumns>,
   length = 200,
-  prefix = 'row-'
+  prefix = '工程师'
 ) =>
   Array.from({ length }).map((_, rowIndex) => {
     return columns.reduce(
       (rowData, column, columnIndex) => {
-        rowData[column.dataKey] = `Row ${rowIndex} - Col ${columnIndex}`
+        /*
+        rowData（每行的数据）: （每行数据）,id,parentId
+        column（每列的属性）: key,dataKey,title,width
+        columnIndex（每列的索引值）: （数字：索引值）
+        */
+        if (column.dataKey == '0') {  // 如果column的dataKey为0号，将其换成工程师名字
+          rowData[column.dataKey] = engineerList[rowIndex];
+        } else {  // 其他日期不变
+          rowData[column.dataKey] = ''
+        }
         return rowData
       },
       {
-        id: `${prefix}${rowIndex}`,
+        id: `${prefix} - ${rowIndex}`,// 每行的ID=“工程师 - （数字）”
         parentId: null,
       }
     )
   })
 
-const columns = generateColumns(10)
-const data = generateData(columns, 200)
+// 工程师列表
+const engineerList = ['cly', 'litZhu', '赵小帅']
 
-const colSpanIndex = 1
-columns[colSpanIndex].colSpan = ({ rowIndex }) => (rowIndex % 4) + 1
+// 列（产生多少列）
+const columns = generateColumns(23)
+// 产生数据（多少列，多少行）
+const data = generateData(columns, engineerList.length)
+
+// 蓝条在哪一列开始显示
+const colSpanIndex = 2
+columns[colSpanIndex].colSpan = ({ rowIndex }) => rowIndex
+
+// 蓝条内文字居中
 columns[colSpanIndex].align = 'center'
 
+const engineerOrders = [
+  {
+    id: 0,
+    orders: [
+      {
+        title: '这是一个工单',
+        startTime: 2,
+        endTime: 3
+      }, {
+        title: '这是一个工单',
+        startTime: 5,
+        endTime: 6
+      }, {
+        title: '这是一个工单',
+        startTime: 9,
+        endTime: 9
+      },
+    ]
+  },
+  {
+    id: 1,
+    orders: [
+      {
+        title: '这是一个工单',
+        startTime: 1,
+        endTime: 5
+      }
+    ]
+  },
+  {
+    id: 2,
+    orders: [
+      {
+        title: '这是一个工单',
+        startTime: 9,
+        endTime: 12
+      }
+    ]
+  },
+]
+
 const Row = ({ rowData, rowIndex, cells, columns }) => {
-  const colSpan = columns[colSpanIndex].colSpan({ rowData, rowIndex })
-  if (colSpan > 1) {
-    let width = Number.parseInt(cells[colSpanIndex].props.style.width)
-    for (let i = 1; i < colSpan; i++) {
-      width += Number.parseInt(cells[colSpanIndex + i].props.style.width)
-      cells[colSpanIndex + i] = null
-    }
-    const style = {
-      ...cells[colSpanIndex].props.style,
-      width: `${width}px`,
-      backgroundColor: 'var(--el-color-primary-light-3)',
-    }
-    cells[colSpanIndex] = cloneVNode(cells[colSpanIndex], { style })
-  }
+  /*
+  rowData（每行的数据）: （每行数据）,id,parentId
+  rowIndex（每行的索引值）: （数字：索引值）
+  cells: 每行的单个数据：数组
+  column（每列的属性）: key,dataKey,title,width
+  */
+
+  engineerOrders.forEach(engineers => {
+    engineers.orders.forEach(order => {
+      console.log(order)
+    })
+  });
+
+  // TODO: 每行的(索引值+1) ????????????????????
+  // const colSpan = columns[colSpanIndex].colSpan({ rowData, rowIndex })
+  // // 如果每行的（索引值+1）大于1
+  // if (colSpan == 0) {// 显示在第一个工程师的行
+  //   // 蓝条宽度
+  //   let width = Number.parseInt(cells[colSpanIndex].props.style.width) * 4
+
+  //   const style = {
+  //     ...cells[colSpanIndex].props.style,
+  //     width: `${width}px`,
+  //     backgroundColor: 'var(--el-color-primary-light-3)',
+  //   }
+  //   cells[colSpanIndex] = cloneVNode(cells[colSpanIndex], { style })
+  // }
 
   return cells
 }
